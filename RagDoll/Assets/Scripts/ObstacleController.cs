542ffd1dee253360115f26b322775e5f34f95cff
Kenneth <alcanamay@gmail.com>
2023-09-27 18:47:46 -0600

    Spawner medianamente funcional

--- RagDoll/Assets/Scripts/ObstacleController.cs
+++ RagDoll/Assets/Scripts/ObstacleController.cs
@@ -11,13 +11,15 @@ public class ObstacleController : MonoBehaviour
   public float radio;
   public List<GameObject> obstacleList;
   public List<Vector3> ObstaclePosition;
+  public float rangoX;
+  public float rangoY;
 
   private void Start()
   {
-    float total = Random.Range(1, 100);
+    float total = Random.Range(1, 10);
     for (int i = 0; i <= total; i++)
     {
-      Vector3 randomPos = new Vector3(Random.Range(-60f, 60f), 1f, Random.Range(-60f,60f));
+      Vector3 randomPos = new Vector3(Random.Range(-rangoX, rangoX), 1f, Random.Range(-rangoY,rangoY));
       if (IsPositionEmpty(randomPos))
       {
         GameObject NewInst = Instantiate(obstacle, randomPos, Quaternion.identity);
